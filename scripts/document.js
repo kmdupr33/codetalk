a847406d9e2f64ad2fcd1e34767e346b38bedba7
Matt Dupree <kmdupr33@gmail.com>
2016-05-17 12:18:39 -0400

    Implement basic progression through sections

---
+++ scripts/document.js
@@ -0,0 +1,20 @@
+(function () {
+    var currentItemIndex = 0;
+    window.addEventListener("keydown", function (event) {
+        if (event.keyCode == 32) {
+            currentItemIndex++;
+            goToSection(currentItemIndex);
+        }
+    });
+    function goToSection(section) {
+        var templates = document.querySelectorAll("template");
+        var clone = document.importNode(templates[section].content, true);
+        var container = document.querySelector("#container");
+        var children = Array.prototype.slice.call(container.childNodes, 0);
+        for (var _i = 0, children_1 = children; _i < children_1.length; _i++) {
+            var child = children_1[_i];
+            child.remove();
+        }
+        container.appendChild(clone);
+    }
+})();
---
+++ scripts/document.ts
@@ -0,0 +1,29 @@
+interface Element {
+  // Add content property to element via declaration merging
+  content: DocumentFragment;
+}
+
+(function() {
+  let currentItemIndex = 0;
+  window.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (event.keyCode == 32) {
+      currentItemIndex++;
+      goToSection(currentItemIndex);
+    }
+  });
+
+  function goToSection(section: number): void {
+
+    let templates = document.querySelectorAll("template");
+    let clone = document.importNode(templates[section].content, true);
+
+    let container = document.querySelector("#container");
+
+    let children = Array.prototype.slice.call(container.childNodes, 0)
+    for (let child of children) {
+      child.remove();
+    }
+
+    container.appendChild(clone);
+  }
+})();
